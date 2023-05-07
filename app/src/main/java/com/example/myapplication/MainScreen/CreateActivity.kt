bddcc213c45e47693bf5c64a9ee81fab3c80b647
JC18JC <jc5467@naver.com>
2023-05-07 20:02:14 +0900

    feat. 방 만들기 페이지 디자인 수정 및 drawble 파일 추가

--- app/src/main/java/com/example/myapplication/MainScreen/CreateActivity.kt
+++ app/src/main/java/com/example/myapplication/MainScreen/CreateActivity.kt
@@ -2,6 +2,10 @@ package com.example.myapplication.MainScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityCreateBinding
 
@@ -12,6 +16,22 @@ class CreateActivity : AppCompatActivity() {
         binding = ActivityCreateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var data = resources.getStringArray(R.array.place)
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.startingSpinner.adapter = adapter
+        /*
+        binding.startingSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val result = data.get(position)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+        */
         /*binding.menuBottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_home -> startActivity(Intent(this, MainActivity::class.java))
