fd23d10fb2f04fcb8c484ef0ff63df9f89468a43
JC18JC <jc5467@naver.com>
2023-05-07 19:47:57 +0900

    fix. 로그인 페이지 오류 해결

--- app/src/main/java/com/example/myapplication/Login/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/Login/LogInActivity.kt
@@ -1,49 +1,12 @@
 package com.example.myapplication.Login
 
-import android.content.ContentValues.TAG
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import com.example.myapplication.MainActivity
-import com.example.myapplication.databinding.ActivityLogInBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.example.myapplication.R
 
 class LogInActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityLogInBinding
-    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLogInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        auth = Firebase.auth
-
-        binding.joinSignUpButton.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
-        }
-        binding.logInBtn.setOnClickListener {
-            var email = binding.idInputEditText.text.toString()
-            var password=binding.pwInputEditText.text.toString()
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(this,"로그인 되었습니다.",Toast.LENGTH_LONG).show()
-                        finish()
-                        startActivity(Intent(this, MainActivity::class.java))
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-        }
+        setContentView(R.layout.activity_log_in)
     }
 }
