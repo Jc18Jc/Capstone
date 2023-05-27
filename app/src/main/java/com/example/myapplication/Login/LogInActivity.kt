d8aa581616ffbb3fbddaba8ba54b69fecda68530
HyunDDeung <gustmd417@gmail.com>
2023-05-27 20:22:56 +0900

    Feat: "구현 : 로그인 및 회원가입"

--- app/src/main/java/com/example/myapplication/Login/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/Login/LogInActivity.kt
@@ -2,21 +2,25 @@ package com.example.myapplication.Login
 
 import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.example.myapplication.Login.SignUpActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.History.HistoryFragment
 import com.example.myapplication.MainActivity
 import com.example.myapplication.databinding.ActivityLogInBinding
-import com.example.myapplication.databinding.ActivitySignUpBinding
+import com.google.api.ResourceDescriptor
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class LogInActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLogInBinding
     private lateinit var auth: FirebaseAuth
+    var userName = ""
+    private val db = FirebaseFirestore.getInstance()    // Firestore 인스턴스
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
@@ -34,9 +38,47 @@ class LogInActivity : AppCompatActivity() {
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(this,"로그인 되었습니다.",Toast.LENGTH_LONG).show()
-                        finish()
-                        startActivity(Intent(this, MainActivity::class.java))
+                        Toast.makeText(this,"로그인 되었습니다.",Toast.LENGTH_SHORT).show()
+
+                        // 로그인된 정보 중에서 해당하는 user의 정보를 찾아서 닉네임을 불러와야 한다.
+                        // 저장된 email을 바탕으로 name을 가져와봅세
+                        Log.d("login", auth.currentUser?.email.toString())
+                        var userEmail = auth.currentUser?.email.toString()
+                        //val data:Any?
+                        db.collection("User").document("$userEmail")
+                            .get().addOnSuccessListener {documentSnapshot ->
+                                if (documentSnapshot.exists()) {
+                                    val data = documentSnapshot.get("name")
+                                    if (data != null) {
+                                        // 데이터가 존재하면 사용 가능
+                                        // 원하는 작업 수행
+                                        Log.d("login_success", data.toString())
+
+                                        /** user의 name정보를 data에 저장했다.
+                                         * 이 데이터를 먼저 mainActivity에 전송해 줄 것이다.*/
+                                        val intent = Intent(this, MainActivity::class.java)
+                                        intent.putExtra("userName", data.toString())
+
+                                        val bundle = Bundle()
+                                        bundle.putString("key_userName", data.toString())
+                                        Log.d("login_success_mainActivity", data.toString())
+                                        val historyFragment = HistoryFragment()
+                                        historyFragment.arguments = bundle
+
+                                        startActivity(intent)
+                                    } else {
+                                        // 데이터가 null인 경우 처리
+                                        Log.d("login_fail", "data is null")
+                                    }
+                                } else {
+                                    // 문서가 존재하지 않는 경우 처리
+                                    Log.d("login_success", "document is not exist")
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                // 불러오기 실패 시 처리
+                                Log.d("login_success", "$e")
+                            }
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "signInWithEmail:failure", task.exception)
--- app/src/main/java/com/example/myapplication/Login/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/Login/SignUpActivity.kt
@@ -2,33 +2,44 @@ package com.example.myapplication.Login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.example.myapplication.DTO.user
 import com.example.myapplication.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySignUpBinding
     private lateinit var auth: FirebaseAuth
+    private val db = FirebaseFirestore.getInstance()    // Firestore 인스턴스
+    var fbAuth : FirebaseAuth? = null
+    var fbFirestore : FirebaseFirestore? = null
+
+    var userName = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = Firebase.auth
+        fbAuth = FirebaseAuth.getInstance()
+        fbFirestore = FirebaseFirestore.getInstance()
 
         var name = binding.nameTextView.text
         var id = ""
         var password1 = ""
         var password2 = ""
 
+        /** backButton action 다사 구현해야 할 수도 */
         binding.backButton.setOnClickListener {
             finish()
         }
 
         binding.signUpButton.setOnClickListener {
-
             id = binding.idTextView.text.toString()
             password1 = binding.passwordTextView1.text.toString()
             password2 = binding.passwordTextView2.text.toString()
@@ -44,6 +55,42 @@ class SignUpActivity : AppCompatActivity() {
                 //즉, 기존에 있는 계정이 아니다
                 else if(task.isSuccessful) {
                     Toast.makeText(this, "회원가입이 완료되었습다.", Toast.LENGTH_SHORT).show()
+
+                    /** 회원가입 된 정보를 firestore에 저장*/
+                    var userInfo = user(id, name.toString(), fbAuth?.uid.toString())
+                    fbFirestore?.collection("User")?.document(id.toString())?.set(userInfo)
+
+                    Log.d("login_signup", auth.currentUser?.email.toString())
+                    var userEmail = auth.currentUser?.email.toString()
+
+                    // 저장된 email을 바탕으로 name을 가져와봅세
+                    db.collection("User").document("$userEmail")
+                        .get().addOnSuccessListener {documentSnapshot ->
+                            if (documentSnapshot.exists()) {
+                                val data = documentSnapshot.data
+                                if (data != null) {
+                                    // 데이터가 존재하면 사용 가능
+                                    // 원하는 작업 수행
+                                    Log.d("signUp_success", data.toString())
+                                } else {
+                                    // 데이터가 null인 경우 처리
+                                    Log.d("signUp_fail", "data is null")
+                                }
+                            } else {
+                                // 문서가 존재하지 않는 경우 처리
+                                Log.d("signUp_success", "document is not exist")
+                            }
+                        }
+                        .addOnFailureListener { e ->
+                            // 불러오기 실패 시 처리
+                            Log.d("signUp_success", "$e")
+
+                        }
+
+
+
+
+
                     finish()
                 }
                 else if (task.exception?.message.isNullOrEmpty()==false) {
